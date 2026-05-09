6bbf239c1382e0739284a6e21c26c58f6b9f6212
Gregory Ord <g.ord@hotmail.ca>
2026-05-09 07:43:49 -0700

    Add RTP proxy to Calian Features I Lead

--- workExperiencePage/setup.js
+++ workExperiencePage/setup.js
@@ -5,6 +5,7 @@ function setupTableRows() {
 }
 
 function setupCalianModals() {
+    PopulatePreModalTitle('Calian/RtpProxy.html', 'RtpProxyTableData')
     PopulatePreModalTitle('Calian/ModernVsCodeOnLegacySystems.html', 'ModernVsCodeTableData')
     PopulatePreModalTitle('Calian/NonDefaultJavaTruststore.html', 'NonDefaultJavaTruststoreTableData')
     PopulatePreModalTitle('Calian/DnsLogger.html', 'DnsLoggerTableData')
